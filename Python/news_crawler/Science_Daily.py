06533309423ade4ae4efe997f81cf84577525b2d
DSchreyer <daniel.schreyer@fau.de>
2019-01-30 16:01:00 +0100

    Extracts Header, Summary, link

--- Python/news_crawler/Science_Daily.py
+++ Python/news_crawler/Science_Daily.py
@@ -23,40 +23,15 @@ class GetArticles():
         r = requests.get(self.url, headers = self.headers)
         doc = BeautifulSoup(r.text, "html.parser")
         for article in doc.select(".col-xs-6.col-md-3"):
+            href = urljoin(self.url, article.find("a").attrs["href"])
             title = article.text
-            print(article.attrs["href"])
+            print(title + "\n" + href)
+            respond = requests.get(href, headers = self.headers)
+            page = BeautifulSoup(respond.text, "html.parser")
+            headline = page.find("h1").text
+            summary = page.find("dd", {"id": "abstract"}).text
+            print(headline)            print(summary)
             break
 
 fetcher = GetArticles("https://www.sciencedaily.com/", headers)
 fetcher.fetch()
-
-
-exit()
-for article in doc.select(".col-xs-6.col-md-3"):
-    print(article)
-    break
-    if article in doc.select(".tab-content"):
-        continue
-    print(article)
-    content = article.select_one(".latest-summary").text
-    header = article.select_one(".latest-head")
-    print(content)
-
-
-exit()
-
-
-def fetch(url):
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3"}
-    reg_url = url
-    req = Request(url=reg_url, headers=headers)
-
-    print(req)
-    time.sleep(1)
-    r = requests.get(req)
-    doc = BeautifulSoup(r.text, "html.parser")
-    print(doc)
-    article = doc.select(".col-xs-6.col-md-3")
-    print(article)
-
-fetch("https://www.sciencedaily.com/")
